4642d197c88ab149d74c129e975b026f6911ff62
KarlKrus <carl_son_cruz@live.com>
2016-12-13 16:50:31 +0800

    12.13.16.04.50.PM

---
+++ EDP/DSManufacturer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+
+namespace EDP
+{
+    public class DSManufacturer
+    {
+        public List<string> ListingEDPbyManufact(List<string> AllEDP)
+        {
+            List<string> EDP = new List<string>();
+            for (int i = 0; i < AllEDP.Count; i++)
+            {
+                string URL = "http://afs-sl-pservice01.afservice.org:8080/productservice2/getProductInfo/pcmall?edplist=" + AllEDP[i] + "&ignoreCatalog=true";
+                XmlTextReader reader = new XmlTextReader(URL);
+                reader.WhitespaceHandling = WhitespaceHandling.Significant;
+
+                while (reader.Read())
+                {
+                    if (reader.Name == "manufacturer")
+                    {
+                        string elementstring = reader.ReadElementString("manufacturer");
+                        EDP.Add(elementstring);
+                    }
+                }
+            }
+            EDP = EDP.Distinct().ToList();
+            return (EDP);
+        }
+    }
+}
--- EDP/SearchedEDP.cs
+++ EDP/SearchedEDP.cs
@@ -2,32 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Xml;
 
 namespace EDP
 {
     public class SearchedEDP
     {
-        public class SearchedEDP
+        public List<string> EDPSearching(string q, string rows)
         {
-            public List<string> EDPSearching(string q, string rows)
+            List<string> EDP = new List<string>();
+            EDP.Clear();
+            string URL = "http://afs-sl-schmgr03.afservice.org:8080/searchManager/search/afs-sl-schmstr.afservice.org:8080/solr4/Products/select?q=" + q + "&fl=EDP&store=pcmall&rows=" + rows + "&start=0";
+            XmlTextReader reader = new XmlTextReader(URL);
+            while (reader.ReadToFollowing("result"))
             {
-                List<string> EDP = new List<string>();
-                EDP.Clear();
-                string URL = "http://afs-sl-schmgr03.afservice.org:8080/searchManager/search/afs-sl-schmstr.afservice.org:8080/solr4/Products/select?q=" + q + "&fl=EDP&store=pcmall&rows=" + rows + "&start=0";
-                XmlTextReader reader = new XmlTextReader(URL);
-                while (reader.ReadToFollowing("result"))
+                while (reader.ReadToFollowing("int"))
                 {
-                    while (reader.ReadToFollowing("int"))
+                    if (reader.GetAttribute("name") == "EDP")
                     {
-                        if (reader.GetAttribute("name") == "EDP")
-                        {
-                            string xmltext_EDP = reader.ReadElementString("int");
+                        string xmltext_EDP = reader.ReadElementString("int");
 
-                            EDP.Add(xmltext_EDP);
-                        }
+                        EDP.Add(xmltext_EDP);
                     }
                 }
-                return (EDP);
-
             }
+            return (EDP);
         }
+    }
+}
