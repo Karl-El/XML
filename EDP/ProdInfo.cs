66b0a4296427e61ea549e8eab4319d0ed1e2d3e7
KarlKrus <carl_son_cruz@live.com>
2016-12-14 10:03:49 +0800

    12.14.16.10.03.AM

---
+++ EDP/ProdInfo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+
+namespace EDP
+{
+    public class ProdInfo
+    {
+
+        public static void ShowDetails(List<string> GetEDP, string NAME, string MANUFACT, string DESC, string FINALPRICE, string AVAILDESC, string IMAGEURL)
+        {
+            #region Working Code for Prod details
+            for (int i = 0; i < GetEDP.Count; i++)
+            #region FORSTART
+            {
+                string URL = "http://afs-sl-pservice01.afservice.org:8080/productservice2/getProductInfo/pcmall?edplist=" + GetEDP[i] + "&ignoreCatalog=true";
+                XmlTextReader reader = new XmlTextReader(URL);
+                reader.WhitespaceHandling = WhitespaceHandling.Significant;
+                while (reader.Read())
+                {
+
+                    if (reader.Name == "name")
+                    {
+                        NAME = reader.ReadElementString("name");
+                    }
+                    if (reader.Name == "manufacturer")
+                    {
+                        MANUFACT = reader.ReadElementString("manufacturer");
+                    }
+                    if (reader.Name == "description")
+                    {
+                        DESC = reader.ReadElementString("description");
+                    }
+                    if (reader.Name == "finalPrice")
+                    {
+                        FINALPRICE = reader.ReadElementString("finalPrice");
+                    }
+                    if (reader.Name == "availabilityDescription")
+                    {
+                        AVAILDESC = reader.ReadElementString("availabilityDescription");
+                    }
+                    if (reader.Name == "xlg")
+                    {
+                        IMAGEURL = reader.ReadElementString("xlg");
+
+                    }
+                }
+                #endregion
+            }
+            #endregion
+        }
+    }
+}
